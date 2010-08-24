26222d56f91eb6ba074ac8ac70f74685c331bb85
Felix Morgner <seuche@Sandbank.local>
2010-08-24 22:46:58 +0200

    added class representing an authorized user

---
+++ Classes/FMAuthorizedUser.m
@@ -0,0 +1,28 @@
+//
+//  FMAuthorizedUser.m
+//  FMTwitterKit
+//
+//  Created by Felix Morgner on 24.08.10.
+//  Copyright (c) 2010 Bühler AG. All rights reserved.
+//
+
+#import "FMAuthorizedUser.h"
+
+
+@implementation FMAuthorizedUser
+
+- (id)init {
+    if ((self = [super init])) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    // Clean-up code here.
+    
+    [super dealloc];
+}
+
+@end
